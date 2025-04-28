30a4064ab38dfe4a31502f2c5fd7d236ab9d7132
user1 <user1@DESKTOP-JN9MOHA>
2025-04-28 17:56:39 +0900

    20250428_after

---
+++ 06_Object/src/com/kh/chap05_method/controller/MethodTest1.java
@@ -0,0 +1,91 @@
+package com.kh.chap05_method.controller;
+
+public class MethodTest1 {
+	/*
+	 * 메서드 작성법
+	 * 접근제한자 [예약어: static, final 같은 것들. 생략 가능.] 반환형 메서드명([매개변수: 생략가능]) {
+	 *     수행할 코드들
+	 *     [return 반환값;] // 반환형이 void인 경우 생략 가능
+	 * } 
+	 * 
+	 */
+	
+	// 1. 반환형이 X, 매개변수가 X
+	public void method1() {
+		System.out.println("매개변수와 반환형이 없는 메서드");
+		
+		// return; // JVM이 자동으로 생성해주기 때문에 생략 가능.
+	}
+	
+	// 2. 반환형 O, 매개변수 X
+	public int method2() {
+		System.out.println("매개변수는 없고, 반환형은 있는 메서드");
+		
+		return 0;
+	}
+	
+	// 3. 반환형 X, 매개변수 O
+	public void method3(int num1, int num2) {
+		System.out.println("매개변수는 있고, 반환형은 없는 메서드");
+		
+		// num1과 num2중 더 큰 값, 작은 값을 출력하는 메서드
+		int min = num1;
+		int max = num2;
+		
+		if(num1 > num2) {
+			max = num1;
+			min = num2;
+		}
+		
+		System.out.println("최소값: " + min + "최대값: " + max);
+	}
+	
+	// 4. 매개변수도 있고, 반환값도 있는 메서드
+	public int method4(int a, int b) {
+		System.out.println("매개변수도 있고, 반환형도 있는 메서드");
+		return a * b;
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 06_Object/src/com/kh/chap05_method/run/Run.java
@@ -0,0 +1,54 @@
+package com.kh.chap05_method.run;
+
+import com.kh.chap05_method.controller.MethodTest2;
+
+public class Run {
+	int count = 0;
+	
+	public static void main(String[] args) {
+//		MethodTest2 mt2 = new MethodTest2();
+//		mt2.method(0); // static 메서드 호출을 위해서, 객체생성은 불필요.
+		
+		// 클래스명, 메서드형
+		// MethodTest2.method(count); 에러남 (static 변수만 써야함)
+		Math.random();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
